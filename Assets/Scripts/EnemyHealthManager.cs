2820d38ed2603ebe2f80cf921f0844473ce05684
Lucas Tai <lucastai.98@gmail.com>
2022-03-21 03:58:57 +0800

    attack animations, controls, hit boxes and enemy hit box, enemy health manager, enemy getting hurt by attack

---
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthManager : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+    private bool flashActive;
+    [SerializeField]
+    private float flashLength = 0f;
+    private float flashCounter = 0f;
+    [SerializeField]
+    private float blinkLength = 0f;
+    private SpriteRenderer enemySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	    enemySprite = GetComponent<SpriteRenderer>();        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flashActive)
+	{
+	    if ((int)(100 * flashCounter / blinkLength) % 2 == 0)
+	    {
+		enemySprite.color = new Color(enemySprite.color.r, enemySprite.color.g, enemySprite.color.b, 0f);
+	    }
+	    else
+	    {
+		enemySprite.color = new Color(enemySprite.color.r, enemySprite.color.g, enemySprite.color.b, 1f);
+	    }
+
+	    if (flashCounter <= 0)
+	    {
+		enemySprite.color = new Color(enemySprite.color.r, enemySprite.color.g, enemySprite.color.b, 1f);
+		flashActive = false;
+	    }
+	    flashCounter -= Time.deltaTime;
+	}
+
+    }
+    public void HurtEnemy(int damageToGive)
+    {
+        currentHealth -= damageToGive;
+        if(currentHealth<=0){
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/HurtEnemy.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemy : MonoBehaviour
+{
+    public int damageToGive = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if ( other.tag == "Enemy")
+        {
+            EnemyHealthManager eHealthMan;
+            eHealthMan = other.gameObject.GetComponent<EnemyHealthManager>();
+            eHealthMan.HurtEnemy(damageToGive);
+            // Destroy(other.gameObject);
+        }
+    }
+}
