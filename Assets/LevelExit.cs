4c7d827cc8306a576036c29031ec289d8e807812
tingalinga <47927589+tingalinga@users.noreply.github.com>
2022-03-20 18:00:34 +0800

    Implemented level loader collider

--- Assets/LevelExit.cs
+++ Assets/LevelExit.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelExit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] float levelExitSlowMoFactor = 0.2f;
+    [SerializeField] string nextLevel;
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        StartCoroutine(LoadScene());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadScene() 
     {
-        
+        Time.timeScale = levelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+
+        SceneManager.LoadScene(nextLevel);
+    }
+
+    IEnumerator LoadNextScene() 
+    {
+        Time.timeScale = levelExitSlowMoFactor;
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        Time.timeScale = 1f;
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
     }
 }
