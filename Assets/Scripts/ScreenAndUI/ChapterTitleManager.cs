7e8db5d59d2b8519fc12f6c2d35cf1b976cdad80
erisjacey-DESKTOP <eris_jacey@hotmail.com>
2022-04-13 22:28:21 +0800

    Misc fix, chapter title stuff

--- Assets/Scripts/ScreenAndUI/ChapterTitleManager.cs
+++ Assets/Scripts/ScreenAndUI/ChapterTitleManager.cs
@@ -18,12 +18,10 @@ public class ChapterTitleManager : MonoBehaviour
 
     private Text text1;
     private Text text2;
-    private Text text3;
 
     private void Awake() {
         text1 = transform.Find("Text1").GetComponent<Text>();
-        text2 = transform.Find("Text2").GetComponent<Text>();
-        text3 = transform.Find("Text3").GetComponent<Text>();       
+        text2 = transform.Find("Text2").GetComponent<Text>(); 
     }
 
     void Start()
@@ -37,18 +35,13 @@ public class ChapterTitleManager : MonoBehaviour
         yield return new WaitForSecondsRealtime(timeTillMessageAppears);
         textWriter.AddWriter(text1, string.Format("Chapter {0}: {1}", chapterNumber, chapterTitle), 0.05f, true);
 
-        // Display text2: event
+        // Display text2: event, time period
         yield return new WaitForSecondsRealtime(messageInterval);
-        textWriter.AddWriter(text2, string.Format("{0}", location), 0.05f, true);
-
-        // Display text3: time period
-        yield return new WaitForSecondsRealtime(messageInterval);
-        textWriter.AddWriter(text3, string.Format("{0}", period), 0.05f, true);
+        textWriter.AddWriter(text2, string.Format("{0}, {1}", location, period), 0.05f, true);
 
         // Disable texts
         yield return new WaitForSecondsRealtime(textTotalDuration);
         text1.gameObject.SetActive(false);
         text2.gameObject.SetActive(false);
-        text3.gameObject.SetActive(false);
     }
 }
